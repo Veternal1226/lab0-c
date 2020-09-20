2215ecf1892a7d5a69f6c66c54fee2244f0be2b2
Veternal1226 <q36084117@gs.ncku.edu.tw>
2020-09-20 09:57:48 +0800

    Implement sort & fix q_size & fix some bug
    Implement sort with merge-sort.
    Add NULL judge of q_size.
    Add update q->tail of insert_tail.

--- queue.c
+++ queue.c
@@ -131,6 +131,9 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
         strncpy(sp, q->head->value, len);
     }
     list_ele_t *rdyrm = q->head;
+    if (q->head == q->tail) {
+        q->tail = NULL;
+    }
     q->head = q->head->next;  // head move to next
     q->size--;
     free(rdyrm->value);
@@ -148,6 +151,8 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        return 0;
     return q->size;
 }
 
@@ -176,7 +181,39 @@ void q_reverse(queue_t *q)
     }
     target->next = prev;
     q->head = target;
+}
 
+
+void merge_sort_rec(list_ele_t **head)
+{
+    if (!(*head) || !(*head)->next)
+        return;
+    list_ele_t *fast = (*head)->next;
+    list_ele_t *slow = *head;
+    while (fast && fast->next) {  // move fast to tail,move slow to middle.
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    list_ele_t *listR = slow->next;  // right-half list
+    slow->next = NULL;               // add NULL to tail of left-half list
+    list_ele_t *listL = *head;
+    merge_sort_rec(&listR);
+    merge_sort_rec(&listL);
+
+    *head = NULL;
+    list_ele_t **newListTail = head;
+
+    while (listL && listR) {
+        if (strcmp(listL->value, listR->value) < 0) {  // L smaller
+            *newListTail = listL;
+            listL = listL->next;
+        } else {
+            *newListTail = listR;
+            listR = listR->next;
+        }
+        newListTail = &((*newListTail)->next);
+    }
+    *newListTail = listL ? listL : listR;
 }
 
 /*
@@ -188,4 +225,14 @@ void q_sort(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || !q->head || q->size <= 1) {
+        return;
+    }
+    merge_sort_rec(&q->head);
+
+    while (q->tail->next) {
+        q->tail = q->tail->next;  // update q->tail to new tail
+    }
+    return;
+
 }
